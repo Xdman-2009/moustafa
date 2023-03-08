26808c521a289597f9f47674b3603cc7a8a6cd32
Xdman-2009 <109619765+Xdman-2009@users.noreply.github.com>
2023-03-08 22:36:13 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,108 @@
+// creating an array and passing the number, questions, options, and answers
+const questions = [
+    {
+    numb: 1,
+    question: "Which of the following statements is true about similar triangles?",
+    answer: "They have the same angles",
+    options: [
+      "They have the same angles",
+      "They have the same side lengths",
+      "They have the same area"
+    ],
+    
+  },
+    {
+    numb: 2,
+    question: "Two triangles are similar if:",
+    answer: "Their corresponding side lengths are congruent",
+    options: [
+      "Their corresponding angles are congruent",
+      "Their corresponding side lengths are congruent",
+      " Both a and b"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the ratio of the lengths of the corresponding sides of two similar triangles?",
+    answer: "The same for all corresponding sides",
+    options: [
+      "The same for all corresponding sides",
+      "Different for each corresponding side",
+      "The ratio is not defined"
+    ]
+  },
+    {
+    numb: 4,
+    question: " Which of the following is not a criterion for proving two triangles similar?",
+    answer: "SAS (side-angle-side)",
+    options: [
+      "AA (angle-angle)",
+      "SAS (side-angle-side)",
+      "AAS (angle-angle-side)"
+    ]
+  },
+    {
+    numb: 5,
+    question: "If two angles of one triangle are congruent to two angles of another triangle, then the triangles are:",
+    answer: "Similar",
+    options: [
+      "Congruent",
+      "Neither congruent nor similar",
+      "Similar"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What two numbers if we add we get '72' and if we multiply we get '1296' ",
+    answer: "36, 36",
+    options: [
+      "24, 24",
+      "12, 12",
+      "54, 54",
+      "36, 36"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What two numbers if we add we get '48' and if we multiply we get '576' ",
+    answer: "24, 24",
+    options: [
+      "56, 56",
+      "14, 14",
+      "32, 32",
+      "24, 24"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What two numbers if we add we get '55' and if we multiply we get '750' ",
+    answer: "25, 30",
+    options: [
+      "71, 4",
+      "25, 2",
+      "21, 3",
+      "25, 30"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What two numbers if we add we get '36' and if we multiply we get '315' ",
+    answer: "15, 21",
+    options: [
+      "74, 9",
+      "31, 5",
+      "63, 5",
+      "15, 21"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following is a property of similar triangles?",
+    answer: "Their corresponding angles are equal",
+    options: [
+      "Their corresponding angles are equal",
+      "Their corresponding side lengths are equal",
+      "Their corresponding altitudes are equal"
+    ]
+  },
+];
